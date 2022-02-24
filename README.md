cf96029519136eb6823eddaaa69b9db0689b4807
dymafr <erwan1309@hotmail.com>
2022-02-24 12:38:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-hluhrv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-hluhrv)
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" v-model="letter" />
+  <h1 v-if="letter === 'A'">A</h1>
+  <h1 v-else-if="letter === 'B'">B</h1>
+  <h1 v-else-if="letter === 'C'">C</h1>
+  <h1 v-else>Autre</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const letter = ref('');
+</script>
+
+<style scoped lang="scss"></style>
